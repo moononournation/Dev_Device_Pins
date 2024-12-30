b6ed4d0a0544950dc2a5adbf720cc0344bf807a7
moononournation <moononournation@gmail.com>
2024-12-30 21:13:16 +0800

    add JSZWY_CYIS Dev Device

---
+++ src/PINS_JSZWY_CYIS.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#define DEV_DEVICE_PINS
+
+// LED
+#define LED_A 4
+
+// Button
+#define BTN_A 0
+
+// Battery ADC
+#define BAT_ADC 37
+
+// Camera
+#define CAMERA_SUPPORTED
+#define CAMERA_PWDN -1
+#define CAMERA_RST -1
+#define CAMERA_XCLK 27
+#define CAMERA_SIOD 2
+#define CAMERA_SIOC 15
+#define CAMERA_Y9 14
+#define CAMERA_Y8 26
+#define CAMERA_Y7 25
+#define CAMERA_Y6 32
+#define CAMERA_Y5 34
+#define CAMERA_Y4 38
+#define CAMERA_Y3 39
+#define CAMERA_Y2 35
+#define CAMERA_VSYNC 13
+#define CAMERA_HREF 12
+#define CAMERA_PCLK 33
+
+// I2C
+#define I2C_SUPPORTED
+#define I2C_SDA_NUM CAMERA_SIOD
+#define I2C_SCL_NUM CAMERA_SIOC
+#define I2C_FREQ 400000UL
+
+// MOTOR
+#define MOTOR
+#define MOTOR_R_A 19
+#define MOTOR_R_B 22
+#define MOTOR_L_A 18
+#define MOTOR_L_B 23
