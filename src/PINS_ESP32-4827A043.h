f6e554ba47489129444939571514c8d315e0c345
moononournation <moononournation@gmail.com>
2025-02-11 22:24:40 +0800

    add I2C and Touchscreen define

--- src/PINS_ESP32-4827A043.h
+++ src/PINS_ESP32-4827A043.h
@@ -19,7 +19,22 @@
 Arduino_DataBus *bus = new Arduino_ESP32QSPI(
     45 /* cs */, 47 /* sck */, 21 /* d0 */, 48 /* d1 */, 40 /* d2 */, 39 /* d3 */);
 Arduino_NV3041A *gfx = new Arduino_NV3041A(bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */, true /* IPS */);
-#define GFX_SPEED 32000000
+#define GFX_SPEED 32000000UL
+
+// I2C
+#define I2C_SUPPORTED
+#define I2C_SDA 8
+#define I2C_SCL 4
+#define I2C_FREQ 400000UL
+
+// Touchscreen
+#define TOUCH_SUPPORTED
+#define TOUCH_MODULES_GT911
+#define TOUCH_MODULE_ADDR GT911_SLAVE_ADDRESS1
+#define TOUCH_SCL I2C_SCL
+#define TOUCH_SDA I2C_SDA
+#define TOUCH_RES 38
+#define TOUCH_INT 3
 
 // I2S output
 #define I2S_OUTPUT
